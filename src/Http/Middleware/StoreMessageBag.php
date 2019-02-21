08bb442ec8b4e9c86104ed25c5c65e3c693e4371
Mior Muhammad Zaki <crynobone@gmail.com>
2019-02-21 08:20:52 +0800

    Test middleware using testbench.
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/Http/Middleware/StoreMessageBag.php
+++ src/Http/Middleware/StoreMessageBag.php
@@ -3,27 +3,10 @@
 namespace Orchestra\Messages\Http\Middleware;
 
 use Closure;
-use Illuminate\Contracts\Foundation\Application;
+use Orchestra\Support\Facades\Messages;
 
 class StoreMessageBag
 {
-    /**
-     * The application implementation.
-     *
-     * @var \Illuminate\Contracts\Foundation\Application
-     */
-    protected $app;
-
-    /**
-     * Create a new middleware instance.
-     *
-     * @param  \Illuminate\Contracts\Foundation\Application  $app
-     */
-    public function __construct(Application $app)
-    {
-        $this->app = $app;
-    }
-
     /**
      * Handle an incoming request.
      *
@@ -34,10 +17,8 @@ class StoreMessageBag
      */
     public function handle($request, Closure $next)
     {
-        $response = $next($request);
-
-        $this->app->make('orchestra.messages')->save();
-
-        return $response;
+        return \tap($next($request), function () {
+            Messages::save();
+        });
     }
 }
--- tests/Feature/Http/Middleware/StoreMessageBagTest.php
+++ tests/Feature/Http/Middleware/StoreMessageBagTest.php
@@ -1,35 +1,27 @@
 <?php
 
-namespace Orchestra\Messages\TestCase\Unit\Http\Middleware;
+namespace Orchestra\Messages\TestCase\Feature\Http\Middleware;
 
 use Mockery as m;
 use Illuminate\Http\Request;
-use PHPUnit\Framework\TestCase;
+use Orchestra\Testbench\TestCase;
+use Orchestra\Support\Facades\Messages;
 use Orchestra\Contracts\Messages\MessageBag;
 use Illuminate\Contracts\Foundation\Application;
 use Orchestra\Messages\Http\Middleware\StoreMessageBag;
 
 class StoreMessageBagTest extends TestCase
 {
-    /**
-     * Teardown the test environment.
-     */
-    protected function tearDown(): void
-    {
-        m::close();
-    }
-
     /** @test */
     public function it_handle_as_http_middleware()
     {
-        $app = m::mock(Application::class);
         $messages = m::mock(MessageBag::class);
         $request = m::mock(Request::class);
 
-        $app->shouldReceive('make')->once()->with('orchestra.messages')->andReturn($messages);
+        $this->app->instance('orchestra.messages', $messages);
         $messages->shouldReceive('save')->once()->andReturnNull();
 
-        $stub = new StoreMessageBag($app);
+        $stub = new StoreMessageBag();
 
         $this->assertEquals('foo', $stub->handle($request, function ($request) {
             return 'foo';
